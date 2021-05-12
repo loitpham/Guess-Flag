96305936e90e67afd1eb454b629c4f72340171cb
Loi Pham <loitpham@gmail.com>
2021-05-12 11:53:43 -0700

    Initial setup.

---
+++ Guess-Flag/Guess-Flag/Guess_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_FlagApp.swift
+//  Guess-Flag
+//
+//  Created by Loi Pham on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Guess_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
